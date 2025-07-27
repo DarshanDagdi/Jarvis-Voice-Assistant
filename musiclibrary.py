b878f746f583ca942874a69c4984fe11a19fe956
DarshanDagdi <darshandagdi@gmail.com>
2025-07-28 03:15:36 +0530

    Add project files (OpenAI key removed)

---
+++ musiclibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "Finding Her": "https://youtu.be/3Cp2QTBZAFQ?si=QUYLf3VK3KlK7EDz",
+    "Pal Pal": "https://youtu.be/8of5w7RgcTc?si=Zc1zRfxNrNgqzag4",
+    "Wishes": "https://youtu.be/Gz38Yj09k3A?si=I8EBhTRzB1YAoDv8",
+    "Jhol": "https://youtu.be/-2RAq5o5pwc?si=y_nMMVoAKJf2YZZX"
+}
